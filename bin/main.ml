0057aec2d4f2ea939673e3fc9126e92ea7bdbad7
Lektonic <lektonic@lektonic.com>
2023-08-03 20:02:53 -0500

    (fix) Fixed stack bytecode in main

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 open Lib
 open Lib.Opcodes.Values
 
-let a = StackAccMachine.run [ pushi; 7; pushi; 23; adds; popa; hlrs ]
+let a = StackAccMachine.run [ pushi; 7; pushi; 23; adds; hlrs ]
 let () = print_endline (string_of_int a)
